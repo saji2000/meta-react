d41da2691d294cc04fcd3af69826ed5c088c95ee
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-03 19:07:15 -0600

    learned event handlers with onMouseOver

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,22 @@
+// function Button() {
+//   const clickHandler = () => console.log("Button clicked");
+//   return (
+//     <button type="button" className="btn btn-primary" onClick={clickHandler}>
+//       Primary
+//     </button>
+//   );
+// }
+
+// export default Button;
+
 function Button() {
-  const clickHandler = () => console.log("Button clicked");
+  const clickHandler = () => console.log("Mouse over button");
   return (
-    <button type="button" className="btn btn-primary" onClick={clickHandler}>
+    <button
+      type="button"
+      className="btn btn-primary"
+      onMouseOver={clickHandler}
+    >
       Primary
     </button>
   );
