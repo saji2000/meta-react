e8889b084660199abf087f2be1de04b072ff0889
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-05 17:55:34 -0600

    Changing variables using event handlers

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,35 @@
+function Button() {
+  var darkModeOn = false;
+  var darkMode = "Dark Mode";
+  var lightMode = "Light Mode";
+  const clickHandler = () => {
+    darkModeOn = !darkModeOn;
+    darkModeOn ? console.log("Dark Mode") : console.log("Light Mode");
+  };
+
+  return (
+    <>
+      <div>{darkModeOn ? darkMode : lightMode}</div>
+      <button type="button" className="btn btn-primary" onClick={clickHandler}>
+        Change Color
+      </button>
+    </>
+  );
+}
+
+// export default Button;
+
 // function Button() {
-//   const clickHandler = () => console.log("Button clicked");
+//   const clickHandler = () => console.log("Mouse over button");
 //   return (
-//     <button type="button" className="btn btn-primary" onClick={clickHandler}>
+//     <button
+//       type="button"
+//       className="btn btn-primary"
+//       onMouseOver={clickHandler}
+//     >
 //       Primary
 //     </button>
 //   );
 // }
-
-// export default Button;
-
-function Button() {
-  const clickHandler = () => console.log("Mouse over button");
-  return (
-    <button
-      type="button"
-      className="btn btn-primary"
-      onMouseOver={clickHandler}
-    >
-      Primary
-    </button>
-  );
-}
 
 export default Button;
