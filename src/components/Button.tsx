259e616e175f6323d85e410b14e85b0e0372d9d1
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-07 17:50:25 -0600

    Learning to use hooks

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,47 +1,24 @@
+import { useState } from "react";
+
 function Button() {
-  var darkModeOn = false;
-  var darkMode = "Dark Mode";
-  var lightMode = "Light Mode";
-  const clickHandler = () => {
-    darkModeOn = !darkModeOn;
-    darkModeOn ? console.log("Dark Mode") : console.log("Light Mode");
+  const [inputText, setText] = useState<string>("");
+
+  const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
   };
-
-  const guessNumber = () => {
-    let num = Math.floor(Math.random() * 3) + 1;
-    let inputNum = prompt("Enter a number: ");
-
-    if (num == Number(inputNum)) {
-      console.log("You won");
-    } else {
-      console.log("You lose");
-    }
-    console.log("The number: " + num + " || Your guess: " + inputNum);
-  };
-
   return (
     <>
-      <div>{darkModeOn ? darkMode : lightMode}</div>
-      <button type="button" className="btn btn-primary" onClick={guessNumber}>
-        Guess
+      <input value={inputText} onChange={handleChanges}></input>
+      <p>This is your text: {inputText}</p>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setText("Null")}
+      >
+        Reset
       </button>
     </>
   );
 }
-
-// export default Button;
-
-// function Button() {
-//   const clickHandler = () => console.log("Mouse over button");
-//   return (
-//     <button
-//       type="button"
-//       className="btn btn-primary"
-//       onMouseOver={clickHandler}
-//     >
-//       Primary
-//     </button>
-//   );
-// }
 
 export default Button;
