93a1aba66072bc1175d467213a565b36669973f3
saji2000 <sajaddaneshmand79@gmail.com>
2023-08-28 11:52:00 -0600

    Video player

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import qr from "./assets/images/QR.png";
+import Video from "./components/Video";
 
 function App() {
   return (
     <>
       <Nav />
       <img height={200} src={qr} alt={"QR code for dicord"} />
+      <Video />
     </>
   );
 }
---
+++ src/components/Video.tsx
@@ -0,0 +1,9 @@
+import ReactPlayer from "react-player/youtube";
+
+const Video = () => {
+  return (
+    <ReactPlayer url="https://www.youtube.com/watch?v=XU8rNrTnIUE&list=RDXU8rNrTnIUE&start_radio=1" />
+  );
+};
+
+export default Video;
