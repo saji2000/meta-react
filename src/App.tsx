8f7315e45656e77864183de87a559b187237428b
saji2000 <sajaddaneshmand79@gmail.com>
2023-09-22 15:07:13 -0600

    working on the lab, thank God

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Nav from "./components/Nav";
-import qr from "./assets/images/QR.png";
-import List from "./components/List";
+import "./styles/main.css";
 import Form from "./components/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      {/* <Nav />
-      <img height={200} src={qr} alt={"QR code for dicord"} />
-      <List /> */}
       <Form />
     </>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,39 +14,57 @@ const Form = () => {
 
   return (
     <>
-      <form>
-        <div className="mb-3">
-          <label>First Name: </label>
-          <input type="text" name="firstName"></input>
-        </div>
+      <div className="container-xxl pt-3 border rounded border-dark-subtle">
+        <form>
+          <div className="mb-3">
+            <label className="form-label">First Name: </label>
+            <input
+              type="text"
+              name="firstName"
+              className="form-control limited-width"
+            ></input>
+          </div>
 
-        <div className="mb-3">
-          <label>Last Name: </label>
-          <input type="text" name="lastName"></input>
-        </div>
+          <div className="mb-3">
+            <label className="form-label">Last Name: </label>
+            <input
+              type="text"
+              name="lastName"
+              className="form-control limited-width"
+            ></input>
+          </div>
 
-        <div className="mb-3">
-          <label>Email: </label>
-          <input type="email" name="email"></input>
-        </div>
+          <div className="mb-3">
+            <label>Email: </label>
+            <input
+              type="email"
+              name="email"
+              className="form-control limited-width"
+            ></input>
+          </div>
 
-        <div className="mb-3">
-          <label>Password: </label>
-          <input type="password" name="password"></input>
-        </div>
+          <div className="mb-3">
+            <label>Password: </label>
+            <input
+              type="password"
+              name="password"
+              className="form-control limited-width"
+            ></input>
+          </div>
 
-        <div className="mb-3">
-          <label>Role: </label>
-          <select>
-            <option value="jobSeeker">Job Seeker</option>
-            <option value="employer">Employer</option>
-          </select>
-        </div>
+          <div className="mb-3">
+            <label>Role: </label>
+            <select className="form-control limited-width">
+              <option value="jobSeeker">Job Seeker</option>
+              <option value="employer">Employer</option>
+            </select>
+          </div>
 
-        <div className="mb-3">
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
+          <div className="mb-3">
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
