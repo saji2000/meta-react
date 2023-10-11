b171394a837ce473eab085efd99cf903624978d8
saji2000 <sajaddaneshmand79@gmail.com>
2023-10-10 23:42:27 -0600

    added comments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,27 @@
+// Import the `useState` hook from the "react" library.
 import { useState } from "react";
 
-// Implementation of a button
+// Implementation of a button component.
 function Button() {
+  // Define a piece of state called `inputText` and a function `setText` to update it.
+  // Initialize `inputText` with an empty string.
   const [inputText, setText] = useState<string>("");
 
+  // Define a function `handleChanges` that updates `inputText` when the input field value changes.
   const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
+  // Render the component with an input field, a paragraph to display the input text, and a reset button.
   return (
     <>
+      {/* Input field that displays and allows editing of `inputText`. */}
       <input value={inputText} onChange={handleChanges}></input>
+
+      {/* Paragraph that displays the current value of `inputText`. */}
       <p>This is your text: {inputText}</p>
+
+      {/* Reset button that sets `inputText` to "Null" when clicked. */}
       <button
         type="button"
         className="btn btn-primary"
@@ -23,4 +33,5 @@ function Button() {
   );
 }
 
+// Export the `Button` component to make it available for use in other parts of the application.
 export default Button;
